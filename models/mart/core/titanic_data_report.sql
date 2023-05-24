412738688e7f3eeb86be43606b7c5adb859b5c3a
Célia Pereira <ccap98@gmail.com>
2023-05-24 15:11:59 +0000

    SEND TO DEV

---
+++ models/mart/core/titanic_data_report.sql
@@ -0,0 +1,33 @@
+with titanic_ref as (
+    
+    select * from {{ ref('stg_titanic') }}
+
+),
+titanic_transformation as(
+SELECT passengerid,sex,
+  CASE 
+    WHEN age is null and sex= 'female' THEN 26
+    WHEN age is null and sex= 'male' THEN 29
+    ELSE age
+  END AS age
+FROM titanic_ref
+)
+,
+final as (
+    select 
+    titanic_ref.PASSENGERID,
+    SURVIVED,
+    PCLASS,
+    NAME,
+    titanic_ref.SEX,
+    titanic_transformation.AGE,
+    SIBSP,
+    PARCH,
+    FARE,
+    EMBARKED
+    from titanic_ref inner join titanic_transformation on titanic_ref.PASSENGERID = titanic_transformation.PASSENGERID
+)
+select 
+    *
+from
+    final
